1c43c514fc5a71907aa3242531f46753130fc750
klelialikothan <up1054352@upnet.gr>
2020-09-05 20:25:31 +0300

    Login screen and password validation styling

--- js/user-locations.js
+++ js/user-locations.js
@@ -14,26 +14,51 @@ $(document).ready(function () {
             console.log("Error: " + xhr.status + ": " + xhr.statusText);
       });
 
-    // map = L.map('map').setView([38.230462, 21.753150], 10);
-    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-    //     maxZoom: 18,
-    //     minZoom: 10
-    // }).addTo(map);
+    map = L.map('map').setView([38.230462, 21.753150], 10);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+        maxZoom: 18,
+        minZoom: 10
+    }).addTo(map);
 
     $('.overlay').on('click', function () {
         $('#sidebar').toggleClass('active');
         $('main').toggleClass('active');
-        // $('.overlay').toggleClass('active');
-        // $('#map').toggleClass('active');
+        $('.overlay').toggleClass('active');
+        $('#map').toggleClass('active');
     });
     
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
         $('main').toggleClass('active');
-        // $('.overlay').toggleClass('active');
-        // $('#map').toggleClass('active');
+        $('.overlay').toggleClass('active');
+        $('#map').toggleClass('active');
     });
 
 });
 
+$("#yearRange").on('click', function () {
+    if ($("#yearRange:checked").length) {
+        $("#endYear").css("visibility", "visible");
+        $(".yearLabels").css("visibility", "visible");
+        $("#startYear option[value='all']").prop("disabled", true);
+    }
+    else {
+        $("#endYear").css("visibility", "hidden");
+        $(".yearLabels").css("visibility", "hidden");
+        $("#startYear option[value='all']").prop("disabled", false);
+    }
+});
+
+$("#monthRange").on('click', function () {
+    if ($("#monthRange:checked").length) {
+        $("#endMonth").css("visibility", "visible");
+        $(".monthLabels").css("visibility", "visible");
+        $("#startMonth option[value='all']").prop("disabled", true);
+    }
+    else {
+        $("#endMonth").css("visibility", "hidden");
+        $(".monthLabels").css("visibility", "hidden");
+        $("#startMonth option[value='all']").prop("disabled", false);
+    }
+});
